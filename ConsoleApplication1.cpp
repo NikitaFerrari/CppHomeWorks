9979e8344b5361ba02716a8e5b88fda3b5ce96f3
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2023-10-02 22:06:08 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,88 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    /*int a;                                         Задача 1 дз2
+    cout << "Write time (seconds): \n";
+    cin  >> a;
+    cout << (a / 3600) << " Hours time \n";
+    cout << (a / 60) << " Minuts time \n";*/
+
+
+    //int a1,a3;
+    //float a2,a;
+    //cin >> a;
+    //a1 = a * 100; // 12.50 = 1250                                    
+    //a2 = (a1 % 100)  ; // получаем центы2
+    //a3 = (a1 / 100) ; // Целые числа
+    //cout << a3 << " Dollars " << a2 << " Cents ";
+
+    /*int a;                                                                 Задача 3 дз2
+    cin >> a;
+    cout << (a / 7) << "Kol-vo nedel \n";
+    cout << (a % 7) << "Kol-vo dney \n";*/
+
+
+    //int a;                                                        Задача 2 дз 1
+    //cin >> a;
+    //int a1 = a % 10; // 4
+    //int a2 = (a % 100) / 10; //3
+    //int a3 = (a / 100) % 10; //2
+    //int a4 = a /1000 ; //1
+    //int a5 = a4*100 + a3*1000 + a1 * 10 +a2 ;
+    //cout << a5;                                                123456
+
+
+    //int a, a1, a2, a3, a4, a5, a6,b,c;
+    //cin >> a;
+    //a1 = (a / 100000); // 1
+    //a2 = (a / 10000) % 10; //2
+    //a3 = (a % 10000) / 1000; //3
+    //a4 = (a % 1000) / 100; //4
+    //a5 = (a % 100) / 10;//5
+    //a6 = (a % 10); //6
+    //b = a1 + a2 + a3;
+    //c = a4 + a5 + a6;
+    //if (b == c) {
+    //    cout << b << " = " << c << "   Pozdravlyayu!!!" << endl;
+    //}
+    //else {
+    //    cout << " Summa pervih treh chisel ne ravna summe poslednih treh";
+    //}
+   
+
+    /*int a, b, c, d, e, f, g;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+    cin >> g;
+    if (a > b)
+        cout << a << "Samoe bolshoe chislo";
+    else if (b > a)
+        cout << b << "Samoe bolshoe chislo";
+    else if (c > a > b > d > e > f > g)
+        cout << c << "Samoe bolshoe chislo";
+    else if (d > a > c > b > e > f > g)
+        cout << d << "Samoe bolshoe chislo";
+    else if (e > a > c > d > b > f > g)
+        cout << e << "Samoe bolshoe chislo";
+    else if (f > a > c > d > e > b > g)
+        cout << f << "Samoe bolshoe chislo";
+    else if (g > a > c > d > e > f > b)
+        cout << g << "Samoe bolshoe chislo";
+    */
+    
+     
+
+
+    
+
+    }
+
+
+
