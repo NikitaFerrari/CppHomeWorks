974a6ca73a6c86b4719b25e49ea73b3be420ce47
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2023-11-08 20:30:44 +0300

    Add files via upload
    Independent creation of the dice game.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,82 +1,71 @@
-﻿
-#include <iostream>
-#include <ctime>
-
+﻿#include <iostream>
 using namespace std;
+
 int main()
 {
     srand(time(NULL));
-    int komp, player;
-    komp = 0;
-    player = 0;
-    int kamen, noz, bumaga;
-    kamen = 1;
-    noz = 2;
-    bumaga = 3;
-    int b;
-    int a;
-    while (komp <= 5 && player <= 5) {
-        srand(time(NULL));
-        cout << " input number from 1 to 3, 1-kamen, 2- noz, 3-bumaga\n";
-        cin >> a;
-        b = rand() % 4;
-        if (a == 1 && b == 2) {
-            ++player;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 1 && b == 1) {
-            cout << "draw"<<endl;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 1 && b == 3) {
-            ++komp;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 2 && b == 1) {
-            ++komp;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 2 && b == 2) {
-            cout << "draw" << endl;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 2 && b == 3) {
-            ++player;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 3 && b == 1) {
-            ++player;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 3 && b == 2) {
-            ++komp;
-            cout << komp << ':' << player << endl;
-        }
-        else if (a == 3 && b == 3) {
-            cout << "draw" << endl;
-            cout << komp << ':' << player << endl;
-        }
-        else {
-            cout << "invalid number"<< endl;
-        }
-    
+    setlocale(LC_ALL, "rus");
+    int playerWin = 0;
+    int pcWin = 0;
+    const int countWins = 3; // кол-во побед
+    /*const int coutAttempts = 5;*/
+    const int minRange = 1; // мин число на кубике
+    const int maxRange = 6; //  макс число на кубике
+    int pcSum, plSum; // сумма выпавших чисел на кубиках
+    int pcNum, plNum, pcNum2, plNum2; // числа на кубиках
 
-    
-    
- 
+    while (playerWin<= countWins && pcWin <= countWins) {
+        system("cls");
+        
+        pcNum = minRange + rand() % (maxRange + 1 - minRange);
+        pcNum2 = minRange + rand() % (maxRange + 1 - minRange);
+        plNum = minRange + rand() % (maxRange + 1 - minRange);
+        plNum2 = minRange + rand() % (maxRange + 1 - minRange);
+        if (plNum = plNum2) {
+            plNum += plNum;
+            plNum2 += plNum2;
+        }
+        else if (pcNum = pcNum2) {
+            pcNum += pcNum;
+            pcNum2 += pcNum2;
+        }
+        plSum = plNum + plNum2;
+        pcSum = pcNum + pcNum2;
+        bool flag = false; //флаг для определения угадано число или нет
+        /*for (int i = 0; i < countWins; i++) {*/
+
+            cout << "общий счет: игрок - " << playerWin << " : пк - " << pcWin << endl;
+            cout << " пользователю выпало: " << plSum << endl;
+            cout << " компьютеру выпало: " << pcSum << endl;
+            if (plSum < pcSum) {
+                pcWin++;
+                cout << "вы проиграли в этом раунде." << endl;
+                /*cout << "общий счет: игрок - " << playerWin << " : пк - " << pcWin << endl;*/
+            }
+            else if (plSum > pcSum) {
+                playerWin++;
+                cout << "вы выиграли в этом раунде." << endl;
+                /*cout << "общий счет: игрок - " << playerWin << " : пк -" << pcWin << endl;*/
+            }
+            if (playerWin >= countWins) {
+                cout << "победил игрок со счетом - " << playerWin << " : " << pcWin << endl;
+                break;
+            }
+            else if (pcWin >= countWins) {
+                cout << "победил компьютер со счетом - " << playerWin << " : " << pcWin << endl;
+                break;
+            
+           
 
 
-    }
-    if (player == 5) {
-        cout << "You win!" << endl;
-    }
-    else {
-        cout << " You lose!:(" << endl;
+            
+        }
+            
+        system("pause");
+        
     }
     
- 
-    
- }
+    }
 
 
 
