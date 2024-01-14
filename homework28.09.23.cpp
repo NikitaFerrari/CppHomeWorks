89e8bc10049879fffdc9c9fdbde982f66b5f0799
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2024-01-14 18:05:24 +0300

    Homework 28.09.2023
    Solving exercises on the topic of division with remainder.

---
+++ homework28.09.23.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// exercise 1
+	/*int time;
+	cout << " Input time in seconds"<< endl;
+	cin >> time;
+	cout << time << "seconds= " << time / 60 << " minutes" << endl;
+	cout << time << "seconds= " << (time / 60)/60 << " hours" << endl;*/
+
+	// exercise 3
+	
+	int days;
+
+	cout << " Input count days"<< endl;
+	cin >> days;
+	cout << days/7 << " weeks and " << days %7 << "days";
+	
+
+}
+
