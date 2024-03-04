ab2d98f34af746d35d8f8bbc056cf11b303ec722
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2024-03-04 21:49:07 +0300

    homework 30.12.cpp

--- homework 30.12.cpp
+++ homework 30.12.cpp
@@ -1,20 +1,75 @@
 ﻿// homework 30.12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+﻿
+
 #include <iostream>
+using namespace std;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(NULL));
+
+    const int STUDENTS = 4;
+    const int MARKS = 5;
+    float avgMarks = 0;
+    float avgDisc = 0;
+    float avgStudents = 0;
+    int best = 0;
+    int sttry = 1;
+    float bStValue = 0;
+    float bStIndex = 0;
+    float wStValue = 0;
+    float wStIndex = 0;
+    float bDiscValue = 0;
+    float IndexBeDi = 0;
+    float wDiscValue = 0;
+    float wDiscIndex = 0;
+    int array[STUDENTS][MARKS]{ {} };
+
+    cout << "\t\t|RUS|\t|MAT|\t|ENG|\t|LIT|\t|GEO|\t|AvgSt|" << endl;
+    for (int i = 0, k = 1; i < STUDENTS; i++) {
+        avgMarks = 0;
+        cout << "Student " << k++ " ";
+        for (int j = 0; j < MARKS; j++) {
+            array[i][j] = 2 + rand() % 4;
+            cout << "\t" "| " << array[i][j] << " |";
+            avgMarks += array[i][j];
+            avgStudents += array[i][j];
+            best = avgMarks;
+        }
+        float avgSt = avgMarks / MARKS;
+        cout << "\t" << "| " << avgSt << " |" << "\t" << endl;
+        if (avgSt < wStValue) {
+            wStValue = avgSt;
+            wStIndex = i;
+        }
+        if (avgSt > bStValue) {
+            bStValue = avgSt;
+            bStIndex = i;
+        }
+    }
+    cout << "AvgDis: ";
+    for (int i = 0; i < MARKS; i++) {
+        avgDisc = 0;
+        for (int j = 0; j < STUDENTS; j++) {
+            avgDisc += array[j][i];
+        }
+        float avgDDisc = avgDisc / MARKS;
+        cout << "\t" << "| " << avgDisc / MARKS << " |";
+        if (avgDDisc < wDiscValue) {
+            wDiscValue = avgDDisc;
+            wDiscIndex = i;
+        }
+        if (avgDDisc > bDiscValue) {
+            bDiscValue = avgDDisc;
+            IndexBeDi = i;
+        }
+    }
+    cout << endl << endl;
+    cout << "students avg: " << avgStudents / (MARKS * STUDENTS) << endl << endl;
+    cout << "The best student: " << bStIndex << endl << endl << "The Words students is: " << wStIndex << endl << endl;
+    cout << "The best discipline: " << IndexBeDi << endl << endl << "The Worst discipline is: " << wDiscIndex << endl << endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
