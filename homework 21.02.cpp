af4bde3dfe619b3d460e4e95b31a234c7373916e
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2024-03-04 21:55:50 +0300

    Work with Functions and Arrays

---
+++ homework 21.02.cpp
@@ -0,0 +1,44 @@
+﻿﻿#include <iostream>
+using namespace std;
+
+#define random(a, b) a+rand()%(b-a+1)
+
+int findindex(int array[], int size, int n) {
+    int index = 0;
+    for (int i = 0; i < size / 2; i++) {
+        if (n == array[i]) return index;
+        index++;
+    }
+    for (int j = size / 2; j < size; j++) {
+        if (n == array[j]) return index;
+        index++;
+    }
+    return -1;
+}
+
+void fielArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = random(1, 10);
+    }
+}
+
+void showArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int chetnArray(int array[], int size, int counter = 0, int i = 0) {
+    if (i == size - 1) return counter;
+    if (array[i++] % 2 == 0) counter++;
+    chetnArray(array, size);
+}
+
+int main()
+{
+    srand(time(NULL));
+    int array[7]{};
+    int size = sizeof(array) / sizeof(array[0]);
+
+}
